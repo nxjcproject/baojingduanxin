cce345538acceffe8b4a0432fa9db2046899d56f
sondacal <673808441@qq.com>
2016-08-08 12:54:47 +0800

    报警短信配置和查询

---
+++ AlarmMessage/AlarmMessage.Service/AlarmMessageSetting/SystemAlarmSettingService.cs
@@ -0,0 +1,239 @@
+﻿using AlarmMessage.Infrastructure.Configuration;
+using SqlServerDataAdapter;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmMessage.Service.AlarmMessageSetting
+{
+    public class  SystemAlarmSettingService
+    {
+        public static DataTable GetSystemAlarmTypeListTable() 
+        {
+            DataTable Table = SystemAlarmType();
+            Table.Columns.Add("selected",typeof(string));
+            Table.Rows.Add("AllValue","全部","true");
+            DataView dv=Table.DefaultView;
+            dv.Sort="AlarmTypeId asc";
+            DataTable table=dv.ToTable();
+            return table;     
+        }
+        private static DataTable SystemAlarmType() 
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+            string mySql = @" SELECT [AlarmTypeId]
+                              ,[AlarmTypeName]                       
+                            FROM [dbo].[system_SystemAlarmType] ";
+            DataTable Table = dataFactory.Query(mySql);
+            return Table;
+        
+        }
+        public static DataTable GetSystemAlarmContrastTable(string organizationID,string AlarmType)
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+            DataTable Table = new DataTable();
+            string mySql = @"SELECT A.[ContrastItemId]
+                            ,A.[StaffInfoItemId]
+                            ,B.[StaffInfoID]
+	                        ,B.Name
+	                        ,B.[PhoneNumber]
+                            ,A.[OrganizationID]
+	                     	,C.Name as Organization
+                            ,A.[MessageType]
+                            ,A.[AlarmTypeId]
+                            ,A.[SendDelay]
+                            ,A.[StartTime]
+                            ,A.[EndTime]
+                            ,A.[AlarmText]
+                            ,A.[Enabled]
+                          FROM [dbo].[terminal_SystemAlarmContrast] A,[dbo].[system_StaffInfo] B,[dbo].[system_Organization] C
+                          where A.[StaffInfoItemId]=B.[StaffInfoItemId]
+                          and A.[OrganizationID]=C.[OrganizationID]
+                          and A.[OrganizationID] like @organizationId+'%' ";
+            if (AlarmType == "AllValue")
+            {
+                mySql = mySql + @" order by B.[StaffInfoID],A.[AlarmTypeId]";
+                Table = dataFactory.Query(mySql, new SqlParameter("@organizationId", organizationID));
+            }
+            else
+            {
+                mySql = mySql + @" and A.[AlarmTypeId]=@alarmType 
+                                   order by B.[StaffInfoID],A.[AlarmTypeId]";
+                SqlParameter[] myPara = { new SqlParameter("@organizationId", organizationID), 
+                                      new SqlParameter("@alarmType", AlarmType) };
+                Table = dataFactory.Query(mySql, myPara);
+
+            } 
+//            string mysql = @"SELECT B.[StaffInfoID]
+//	                        ,B.Name
+//	                        ,B.[PhoneNumber]
+//                            ,A.[OrganizationID]
+//	                      	,C.Name as Organization
+//                            ,A.[MessageType]
+//                            ,A.[AlarmTypeId]
+//                            ,A.[SendDelay]
+//                            ,A.[StartTime]
+//                            ,A.[EndTime]
+//                            ,A.[AlarmText]
+//                            ,A.[Enabled]
+//                          FROM BaiYin.[NXJC].[dbo].[terminal_SystemAlarmContrast] A,BaiYin.[NXJC].[dbo].[system_StaffInfo] B,BaiYin.[NXJC].[dbo].[system_Organization] C
+//                          where A.[StaffInfoItemId]=B.[StaffInfoItemId]
+//                          and A.[OrganizationID]=C.[OrganizationID]
+//                          order by B.[StaffInfoID],A.[AlarmTypeId]";
+//            Table = dataFactory.Query(mysql);
+            return Table;    
+        
+        }
+        public static DataTable GetStaffTable(string organizationID) 
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+            DataTable Table = new DataTable();
+            string mysql = @"SELECT  A.[StaffInfoID]
+	                          ,A.[Name]
+	                          ,A.[StaffInfoItemId]
+	                          ,A.[PhoneNumber]
+                              ,A.[OrganizationID]
+	                          ,B.Name as Organization
+                              ,A.[WorkingTeamName]
+                              ,A.[WorkingSectionID]
+                              ,A.[Sex]
+                              ,A.[Enabled]
+                          FROM [dbo].[system_StaffInfo] A,[dbo].[system_Organization] B
+                          where A.[Enabled]=1
+                          and A.[OrganizationID]=B.[OrganizationID]
+                          and A.[OrganizationID] like @mOrganizationID+'%'
+                          order by Convert(int,A.[StaffInfoID]) asc";
+            Table = dataFactory.Query(mysql, new SqlParameter("@mOrganizationID", organizationID));
+            return Table;    
+        }
+        public static int AddSystemAlarmStaffInfoToTable(string isStaffInfoInsert, string contrastItemId, string organizationID, string alarmType, string alarmTypeName, string phoneNumber, string staffInfoItemId, string beginTime, string endTime, string delay, string enabled)       
+        {
+            int executeResult = 0;
+            if (Convert.ToBoolean(isStaffInfoInsert))
+            {
+                if (alarmType != "AllValue")
+                {
+                    executeResult = InsertOperation(organizationID, alarmType, alarmTypeName, phoneNumber, staffInfoItemId, beginTime, endTime, delay, enabled);
+                }
+                else
+                {
+                    DataTable Table = SystemAlarmType();
+                    string malarmType = "";
+                    string malarmTypeName = "";
+                    foreach (DataRow dR in Table.Rows)
+                    {
+                        malarmType = dR["AlarmTypeId"].ToString();
+                        malarmTypeName = dR["AlarmTypeName"].ToString();
+                        executeResult = executeResult + InsertOperation(organizationID, malarmType, malarmTypeName, phoneNumber, staffInfoItemId, beginTime, endTime, delay, enabled);
+                    }
+                }
+            }
+            else 
+            {
+                executeResult = UpdateOperation(contrastItemId, staffInfoItemId, alarmType, alarmTypeName, beginTime, endTime, delay, enabled);
+            }
+          
+            return executeResult;
+        }
+        private static int InsertOperation(string organizationID, string alarmType, string alarmTypeName, string phoneNumber, string staffInfoItemId, string beginTime, string endTime, string delay, string enabled)
+        {
+            int executeResult = 0;
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string mGUID = System.Guid.NewGuid().ToString();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = @"INSERT INTO [dbo].[terminal_SystemAlarmContrast]
+                                    ([ContrastItemId]
+                                    ,[StaffInfoItemId]
+                                    ,[OrganizationID]
+                                    ,[MessageType]
+                                    ,[AlarmTypeId]
+                                    ,[SendDelay]
+                                    ,[StartTime]
+                                    ,[EndTime]
+                                    ,[AlarmText]
+                                    ,[Enabled])
+                                VALUES
+                                    (@mContrastItemId
+                                    ,@mStaffInfoItemId
+                                    ,@mOrganizationID
+                                    ,@mMessageType
+                                    ,@mAlarmTypeId
+                                    ,@mSendDelay
+                                    ,@mStartTime
+                                    ,@mEndTime
+                                    ,@mAlarmText
+                                    ,@mEnabled)";            
+                command.Parameters.Add(new SqlParameter("@mContrastItemId", mGUID));
+                command.Parameters.Add(new SqlParameter("@mStaffInfoItemId", staffInfoItemId));
+                command.Parameters.Add(new SqlParameter("@mOrganizationID", organizationID));
+                command.Parameters.Add(new SqlParameter("@mMessageType", "SMS"));
+                command.Parameters.Add(new SqlParameter("@mAlarmTypeId", alarmType));
+                command.Parameters.Add(new SqlParameter("@mSendDelay", delay));
+                command.Parameters.Add(new SqlParameter("@mStartTime", beginTime));
+                command.Parameters.Add(new SqlParameter("@mEndTime", endTime));
+                command.Parameters.Add(new SqlParameter("@mAlarmText", alarmTypeName));
+                command.Parameters.Add(new SqlParameter("@mEnabled", enabled));                             
+                connection.Open();
+                executeResult = command.ExecuteNonQuery();      
+            }
+               return executeResult;
+        }
+        private static int UpdateOperation(string contrastItemId, string staffInfoItemId, string malarmType, string malarmTypeName, string beginTime, string endTime, string delay, string enabled)
+        {
+            int executeResult = 0;
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            { 
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = @"UPDATE [dbo].[terminal_SystemAlarmContrast]
+                                       SET 
+                                           [StaffInfoItemId] = @mStaffInfoItemId
+                                          ,[SendDelay] = @mSendDelay
+                                            ,[AlarmTypeId]=@mAlarmTypeId
+                                            ,[AlarmText]=@mAlarmText
+                                          ,[StartTime] = @mStartTime
+                                          ,[EndTime] = @mEndTime
+                                          ,[Enabled] = @mEnabled
+                                     WHERE  [ContrastItemId] =@mContrastItemId";
+                command.Parameters.Add(new SqlParameter("@mContrastItemId", contrastItemId));
+                command.Parameters.Add(new SqlParameter("@mStaffInfoItemId", staffInfoItemId));
+                command.Parameters.Add(new SqlParameter("@mAlarmTypeId", malarmType));
+                command.Parameters.Add(new SqlParameter("@mAlarmText", malarmTypeName));
+                command.Parameters.Add(new SqlParameter("@mSendDelay", delay));
+                command.Parameters.Add(new SqlParameter("@mStartTime", beginTime));
+                command.Parameters.Add(new SqlParameter("@mEndTime", endTime));
+                command.Parameters.Add(new SqlParameter("@mEnabled", enabled));
+                connection.Open();
+                executeResult = command.ExecuteNonQuery();
+            }
+            return executeResult;
+        }
+        public static int CancelSystemAlarmStaffInfoToTable(string contrastItemId) 
+        {
+            int executeResult = 0;
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                string mGUID = System.Guid.NewGuid().ToString();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = @"DELETE FROM [dbo].[terminal_SystemAlarmContrast]
+                                                WHERE [ContrastItemId]=@mContrastItemId";
+                command.Parameters.Add(new SqlParameter("@mContrastItemId", contrastItemId));           
+                connection.Open();
+                executeResult = command.ExecuteNonQuery();
+            }
+            return executeResult;       
+        }
+    }
+}
---
+++ AlarmMessage/AlarmMessage.Web/UI_AlarmMessageSetting/SystemAlarmSetting.aspx.cs
@@ -0,0 +1,67 @@
+﻿using AlarmMessage.Service.AlarmMessageSetting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AlarmMessage.Web.UI_AlarmMessageSetting
+{
+    public partial class SystemAlarmSetting : WebStyleBaseForEnergy.webStyleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            base.InitComponts();
+            if (!IsPostBack)
+            {
+#if DEBUG
+                ////////////////////调试用,自定义的数据授权
+
+                List<string> m_DataValidIdItems = new List<string>() { "zc_nxjc_byc_byf", "zc_nxjc_qtx", "zc_nxjc_tsc_tsf" };
+                AddDataValidIdGroup("ProductionOrganization", m_DataValidIdItems);
+#elif RELEASE
+#endif
+                this.OrganisationTree_ProductionLine.Organizations = GetDataValidIdGroup("ProductionOrganization");                         //向web用户控件传递数据授权参数
+                this.OrganisationTree_ProductionLine.PageName = "SystemAlarmSetting.aspx";   //向web用户控件传递当前调用的页面名称
+                this.OrganisationTree_ProductionLine.LeveDepth = 5;
+            }
+        }
+        [WebMethod]
+        public static string SystemAlarmTypeList() 
+        {
+            DataTable table = SystemAlarmSettingService.GetSystemAlarmTypeListTable();
+            string json=EasyUIJsonParser.DataGridJsonParser.DataTableToJson(table);
+            return json;
+        }
+        [WebMethod]
+        public static string GetSystemAlarmContrast(string organizationID,string AlarmType)
+        {
+            DataTable table = SystemAlarmSettingService.GetSystemAlarmContrastTable(organizationID,AlarmType);
+            string json=EasyUIJsonParser.DataGridJsonParser.DataTableToJson(table);
+            return json;      
+        }
+        [WebMethod]
+        public static string GetStaffTableData(string organizationID) 
+        {
+            DataTable table = SystemAlarmSettingService.GetStaffTable(organizationID);
+            string json = EasyUIJsonParser.DataGridJsonParser.DataTableToJson(table);
+            return json;          
+        }
+        [WebMethod]
+        public static int AddSystemAlarmStaffInfo(string isStaffInfoInsert,string contrastItemId,string organizationID, string alarmType, string alarmTypeName,string phoneNumber, string staffInfoItemId, string beginTime, string endTime, string delay, string enabled)
+            //'{organizationID: "' + organizationId + '",staffId:"' + mStaffInfoID + '",phoneNumber:"' + mPhoneNumber + '",staffInfoItemId:"' + mStaffInfoItemId + '",beginTime:"' + mBeginTime + '",endTime:"' + mEndTime + '",delay:"' + mdelay + '",enabled:"' + mEnabled + '"}',
+        {
+            int reback = SystemAlarmSettingService.AddSystemAlarmStaffInfoToTable(isStaffInfoInsert, contrastItemId,organizationID, alarmType, alarmTypeName, phoneNumber, staffInfoItemId, beginTime, endTime, delay, enabled);
+            return reback;
+        }
+        [WebMethod]
+        public static int CancelSystemAlarmStaffInfo(string contrastItemId) 
+        {
+            int reback = SystemAlarmSettingService.CancelSystemAlarmStaffInfoToTable(contrastItemId);
+            return reback;       
+        }
+    }
+}
